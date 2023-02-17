3789fc43d5ec46e358e978ebc76991af10f5086b
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-17 10:35:16 +0530

    import post lists component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Counter from "./components/Counter/Counter";
+import PostLists from "./components/Posts/PostLists";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <PostLists />
     </div>
   );
 }
