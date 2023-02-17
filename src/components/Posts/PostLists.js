449e1d831d15cf51421571d6759995974f7b6305
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-17 12:59:13 +0530

    order post by recent posts

--- src/components/Posts/PostLists.js
+++ src/components/Posts/PostLists.js
@@ -3,10 +3,15 @@ import { useSelector } from "react-redux";
 import { allPosts } from "../../redux/postsSlice";
 import PostAuthor from "./Author/PostAuthor";
 import CreatePosts from "./createPosts/CreatePosts";
+import PostTime from "./TimeStamp/PostTime";
 
 const PostLists = () => {
   const posts = useSelector(allPosts);
-  const Posts = posts?.map((post) => {
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const Posts = orderedPosts?.map((post) => {
     return (
       <>
         <article key={post.id}>
@@ -14,6 +19,7 @@ const PostLists = () => {
           <p>{post.content.substring(0, 100)}</p>
           <p className="postCredit">
             <PostAuthor userId={post.userId} />
+            <PostTime timestamp={post.date} />
           </p>
         </article>
       </>
