b3c8e7ba04c58fb64781a203bfefbe7ec0768ca4
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-17 12:34:40 +0530

    import it to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
 import postsReducer from "./postsSlice";
+import usersReducer from "./usersSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
     posts: postsReducer,
+    users: usersReducer,
   },
 });
