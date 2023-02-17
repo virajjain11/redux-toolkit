2b47744217f0338c1ae0a897267dde19693ad0da
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-17 10:35:59 +0530

    import posts reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
+import postsReducer from "./postsSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
+    posts: postsReducer,
   },
 });
