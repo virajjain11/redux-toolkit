13af89d7d8f23ace835e9a879f4a3eb33b8a13e4
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-18 10:57:12 +0530

    make post req using api to add new posts

--- src/components/Posts/createPosts/CreatePosts.js
+++ src/components/Posts/createPosts/CreatePosts.js
@@ -1,7 +1,7 @@
 import { nanoid } from "@reduxjs/toolkit";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createPost } from "../../../redux/postsSlice";
+import { addNewPost, createPost } from "../../../redux/postsSlice";
 import { users } from "../../../redux/usersSlice";
 
 const CreatePosts = () => {
@@ -10,21 +10,32 @@ const CreatePosts = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
+  const [addReqStatus, setAddReqStatus] = useState("idle");
 
   const usersList = useSelector(users);
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
+
+  const canSave =
+    [title, content, userId].every(Boolean) && addReqStatus === "idle";
   const savePost = () => {
-    if (title && content) {
-      dispatch(createPost({ title, content, userId }));
-      setTitle("");
-      setContent("");
-      setUserId("");
+    if (canSave) {
+      try {
+        setAddReqStatus("pending");
+        setTitle("");
+        setContent("");
+        setUserId("");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+      } catch (error) {
+        console.error("post not added", error.message);
+      } finally {
+        setAddReqStatus("idle");
+      }
     }
   };
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+  // const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
   const usersOptions = usersList.map((user) => (
     <option key={user.id} value={user.id}>
