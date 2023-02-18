4b723683e98d2b2990773e5d16d46eda0a86f2fc
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-18 10:32:40 +0530

    get users

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./redux/store";
 import "./index.css";
-
+import { fetchUsers } from "./redux/usersSlice";
+store.dispatch(fetchUsers());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
