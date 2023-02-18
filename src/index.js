6be25ffa34798b7ce9c7fb1e9ff413e6846992ed
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-18 15:16:27 +0530

    react router setup

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import App from "./App";
 import { store } from "./redux/store";
 import "./index.css";
 import { fetchUsers } from "./redux/usersSlice";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import {}
+
 store.dispatch(fetchUsers());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
   </Provider>
 );
