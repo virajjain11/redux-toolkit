ff2f0922560f10caebd336563d6f30c1dcdac586
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-18 10:31:21 +0530

    get users from api

--- src/redux/usersSlice.js
+++ src/redux/usersSlice.js
@@ -1,15 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: "0", name: "Dude Lebowski" },
-  { id: "1", name: "Neil Young" },
-  { id: "2", name: "Dave Gray" },
-];
+const initialState = [];
+let POST_URI = "https://jsonplaceholder.typicode.com/users";
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  return axios
+    .get(POST_URI)
+    .then((res) => res.data)
+    .catch((err) => err.response);
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state, action) => {})
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {});
+  },
 });
 // export const {}= usersSlice.actions
 export const users = (state) => state.users;
