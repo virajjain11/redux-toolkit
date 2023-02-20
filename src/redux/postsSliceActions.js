212290b3eb77b2bbbf77a725484ed2f44dd51f65
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-20 12:30:50 +0530

    add edit functionality

--- src/redux/postsSliceActions.js
+++ src/redux/postsSliceActions.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { initialReactionsState } from "./postsSlice";
 
 let POST_URI = "https://jsonplaceholder.typicode.com/posts";
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
@@ -22,3 +23,22 @@ export const updatePost = createAsyncThunk("posts/updatePost", async (post) => {
     .then((res) => res.data)
     .catch((err) => err.message);
 });
+
+// export const addNewPostReducer = (state, action) => {
+//   console.log("hereee", action.payload);
+//   action.payload.userId = +action.payload.userId;
+//   action.payload.date = new Date().toISOString();
+//   action.payload.reactions = initialReactionsState;
+//   state.posts.push(action.payload);
+// };
+export const updatePostReducer = (state, action) => {
+  if (!action.payload?.id) {
+    console.log("Update could not complete");
+    console.log(action.payload);
+    return;
+  }
+  const { id } = action.payload;
+  action.payload.date = new Date().toISOString();
+  const posts = state.posts.filter((post) => post.id !== id);
+  state.posts = [...posts, action.payload];
+};
