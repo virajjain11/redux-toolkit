d7ca89b4ed1f0cc61c86a39b43bf918077d0c62a
Viraj Jain <virajjain7.vj@gmail.com>
2023-02-20 12:53:31 +0530

    code refactor

--- src/components/Posts/PostLists.js
+++ src/components/Posts/PostLists.js
@@ -1,19 +1,12 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { allPosts, postStatus, error } from "../../redux/postsSlice";
-import { fetchPosts } from "../../redux/postsSliceActions";
+
 import AllPosts from "./AllPosts/AllPosts";
 
 const PostLists = () => {
-  const dispatch = useDispatch();
-
   const posts = useSelector(allPosts);
   const status = useSelector(postStatus);
   const errorStatus = useSelector(error);
-
-  useEffect(() => {
-    if (status === "idle") dispatch(fetchPosts());
-  }, [status, dispatch]);
 
   let content;
   if (status === "loading") content = <p>Loading...</p>;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import { store } from "./redux/store";
 import "./index.css";
 import { fetchUsers } from "./redux/usersSlice";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { fetchPosts } from "./redux/postsSliceActions";
 // import {}
 
 store.dispatch(fetchUsers());
+store.dispatch(fetchPosts());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
